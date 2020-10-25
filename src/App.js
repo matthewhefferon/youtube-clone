1e8651b5d30d00b0a07ee027aeaebeeed9fe17ef
Matthew Hefferon <matthewhefferon@Matthews-MacBook-Air.local>
2020-10-24 19:29:54 -0600

    build the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>YouTube Clone</h1>
+    <div className="app">
+      <Header />
     </div>
   );
 }
