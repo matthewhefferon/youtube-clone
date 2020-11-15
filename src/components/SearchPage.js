79a6e90cc4a656f22c2be151965f2a3868bf3938
Matthew Hefferon <matthewhefferon@Matthews-MacBook-Air.local>
2020-11-14 20:36:27 -0700

    video row component and some styling updates

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,7 +1,8 @@
 import React from "react";
-import "../styles/SearchPage.css"
-import TuneSharpIcon from '@material-ui/icons/TuneSharp';
+import "../styles/SearchPage.css";
+import TuneSharpIcon from "@material-ui/icons/TuneSharp";
 import ChannelRow from "./ChannelRow";
+import VideoRow from "./VideoRow";
 
 function SearchPage() {
   return (
@@ -11,15 +12,69 @@ function SearchPage() {
         <h2>FILTER</h2>
       </div>
       <hr />
-      <ChannelRow 
-      image="https://yt3.ggpht.com/a/AATXAJxJPBN1o3Mj7TEEmBx0eyNPq0_uj25m00B02Rl4=s176-c-k-c0x00ffffff-no-rj-mo"
-      channel="Viztip"
-      verified
-      subs="1k"
-      noOfVideos={55}
-      description="Quick video tutorials on all things Tableau."
+      <ChannelRow
+        image="https://yt3.ggpht.com/a/AATXAJxJPBN1o3Mj7TEEmBx0eyNPq0_uj25m00B02Rl4=s176-c-k-c0x00ffffff-no-rj-mo"
+        channel="Viztip"
+        verified
+        subs="1k"
+        noOfVideos={55}
+        description="Quick video tutorials on all things Tableau."
       />
       <hr />
+      <VideoRow
+        views="37"
+        subs="600"
+        description="This quick Tableau tutorial will show you how to add a progress shadow to your bar charts. Please like and subscribe if you find these videos helpful!"
+        timestamp="3 days ago"
+        channel="viztip"
+        title="Tableau Tutorial - Progress Shadow"
+        image="https://i.ytimg.com/an_webp/VGkWRdiUYps/mqdefault_6s.webp?du=3000&sqp=CImvwv0F&rs=AOn4CLAXSW69R05Iz5zH99bUJpt_f6BFdQ"
+      />
+      <VideoRow
+        views="32k"
+        subs="600"
+        description="Quick Tableau tutorial on Top N & Bottom N. Please like and subscribe if you find these videos helpful!"
+        timestamp="2 years ago"
+        channel="viztip"
+        title="Tableau Tutorial - Top N & Bottom N"
+        image="https://i.ytimg.com/an_webp/C72JHMjf-h0/mqdefault_6s.webp?du=3000&sqp=CI25wv0F&rs=AOn4CLAX8dwvMoWgmhrxJMFNk5V1Y72XQw"
+      />
+      <VideoRow
+        views="24k"
+        subs="600"
+        description="Quick tableau tutorial on removing the 'Abc' column from a text table. Please like and subscribe if you find these videos helpful!"
+        timestamp="2 years ago"
+        channel="viztip"
+        title="Tableau Tutorial - Removing 'Abc' Column"
+        image="https://i.ytimg.com/vi/6FTZ6TnYaF0/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCS_MneEwzm2-UWQTYqXFzkpFrd3A"
+      />
+      <VideoRow
+        views="37"
+        subs="600"
+        description="This quick Tableau tutorial will show you how to add a progress shadow to your bar charts. Please like and subscribe if you find these videos helpful!"
+        timestamp="3 days ago"
+        channel="viztip"
+        title="Tableau Tutorial - Progress Shadow"
+        image="https://i.ytimg.com/an_webp/VGkWRdiUYps/mqdefault_6s.webp?du=3000&sqp=CImvwv0F&rs=AOn4CLAXSW69R05Iz5zH99bUJpt_f6BFdQ"
+      />
+      <VideoRow
+        views="32k"
+        subs="600"
+        description="Quick Tableau tutorial on Top N & Bottom N. Please like and subscribe if you find these videos helpful!"
+        timestamp="2 years ago"
+        channel="viztip"
+        title="Tableau Tutorial - Top N & Bottom N"
+        image="https://i.ytimg.com/an_webp/C72JHMjf-h0/mqdefault_6s.webp?du=3000&sqp=CI25wv0F&rs=AOn4CLAX8dwvMoWgmhrxJMFNk5V1Y72XQw"
+      />
+      <VideoRow
+        views="24k"
+        subs="600"
+        description="Quick tableau tutorial on removing the 'Abc' column from a text table. Please like and subscribe if you find these videos helpful!"
+        timestamp="2 years ago"
+        channel="viztip"
+        title="Tableau Tutorial - Removing 'Abc' Column"
+        image="https://i.ytimg.com/vi/6FTZ6TnYaF0/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCS_MneEwzm2-UWQTYqXFzkpFrd3A"
+      />
     </div>
   );
 }
